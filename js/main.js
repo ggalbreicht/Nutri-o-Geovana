6a703b912c1cd288d23d905944c785b6c0cf94bc
ggalbreicht <163944744+ggalbreicht@users.noreply.github.com>
2024-11-12 00:51:38 +0000

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var titulo = document.querySelector("h1");
+titulo.textContent = "Geovana nutricionista";
+
+var subtitulo = document.querySelector("h2");
+subtitulo.textContent = "Pacientes cadastrados";
+
+var paciente = document.querySelector("#primeiro-paciente");
+console.log(paciente);
